dac3ff640ca09f9dd18ce45ab84f188da60b1de6
Ailton Muniz Santos Junior <ailton.msj@gmail.com>
2019-12-15 11:51:47 -0300

    Added new methods

--- src/main/java/br/com/amsj/forum/controller/TopicoController.java
+++ src/main/java/br/com/amsj/forum/controller/TopicoController.java
@@ -1,8 +1,10 @@
 package br.com.amsj.forum.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,22 @@ public class TopicoController {
 	public List<TopicoDto> lista(){
 		
 		List<Topico> topicos = topicoRepository.findAll();		
+		
+		return TopicoDto.converter(topicos);
+	}
+	
+	@RequestMapping("/topicos/{idTopico}")
+	public TopicoDto get(@PathVariable Long idTopico){
+		
+		Optional<Topico> topico = topicoRepository.findById(idTopico);
+		
+		return TopicoDto.converter(topico.get());
+	}
+	
+	@RequestMapping("/topicos/byCursoName/{nomeCurso}")
+	public List<TopicoDto> get(@PathVariable String nomeCurso){
+		
+		List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
 		
 		return TopicoDto.converter(topicos);
 	}
--- src/main/java/br/com/amsj/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/amsj/forum/repository/TopicoRepository.java
@@ -1,10 +1,14 @@
 package br.com.amsj.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.amsj.forum.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCurso_Nome(String nomeCurso);
 	
 	
 
